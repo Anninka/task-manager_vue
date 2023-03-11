2954c0ef827f4beedd5530511a7f05531d831b1e
Anninka <annettech@yandex.ru>
2023-03-11 21:56:20 +1000

    feat: Добавляет разметку и стили колонок и задач. Добавляет массив данных задач и отображает их в колонке (пока без сортировки по статусу)

--- src/App.vue
+++ src/App.vue
@@ -7,7 +7,28 @@
     </header>
     <main class="container">
       <div class="task-desk">
-
+        <div class="task-list">
+          <div class="task-list__header">Создана</div>
+          <div 
+          class="task-item"
+          :key="task"
+          v-for="task in tasks"
+          >
+            <p class="">Название: {{ task.title }}</p>
+            <p class="">Описание: {{ task.description }}</p>
+            <p class="">Автор: {{ task.author }}</p>
+            <p class="">Дата создания: {{ task.date }}</p>
+            <p class="">Взяли в работу:</p>
+            <p class="">Затрачено времени: {{ task.time }}</p>
+            <p class="">Состояние: {{ task.status }}</p>
+          </div>
+        </div>
+        <div class="task-list">
+          <div class="task-list__header">В работе</div>
+        </div>
+        <div class="task-list">
+          <div class="task-list__header">Завершена</div>
+        </div>
       </div>
     </main>
     <footer class="page-footer">
@@ -22,7 +43,43 @@
 <script>
 
 export default {
+  components: {
 
+  },
+  data() {
+    return {
+      tasks: [
+        {
+          id: 1, 
+          title: "Удаление выполненных задач", 
+          description: "Добавить функционал удаления задач из колонки 'Завершена'",
+          author: "Терентьева",
+          date: "13.03.2023 10:10",
+        },
+        {
+          id: 2, 
+          title: "Перетаскивание задач", 
+          description: "Добавить возможность перетаскивать задачи между колонками создана/в работе/завершена",
+          author: "Петров",
+          date: "12.03.2023 12:55",
+        },
+        {
+          id: 3, 
+          title: "Добавление задач в таск-менеджер", 
+          description: "Реализовать добавление новых задач в таск-менеджере по шаблону. Все поля обязательные.",
+          author: "Иванов",
+          date: "10.03.2023 11:33",
+        },
+        {
+          id: 4, 
+          title: "Создание новой карточки", 
+          description: "Необходимо провести следующие операции",
+          author: "Сидорова",
+          date: "09.03.2023 10:05",
+        },
+      ],
+    }
+  },
 }
 </script>
 
@@ -61,7 +118,7 @@ export default {
   display: grid;
   grid-template-columns: 1fr 1fr 1fr;
   gap: 30px;
-  height: calc(100vh - 236px);
+  height: calc(100vh - 220px);
   padding: 30px;
 }
 
@@ -73,5 +130,22 @@ export default {
 .page-footer_text {
   font-size: 16px;
   line-height: 20px;
+}
+
+.task-list {
+  padding: 10px;
+  background-color: #a1c2cb;
+  border-radius: 6px;
+  overflow: auto;
+}
+.task-list__header {
+  height: 30px;
+}
+
+.task-item {
+  margin-bottom: 10px;
+  padding: 10px;
+  background-color: white;
+  border-radius: 5px;
 }
 </style>
